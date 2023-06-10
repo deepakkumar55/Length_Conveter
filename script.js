e9718fcd9004254896b43c60747618c8dc7f9d46
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-06-10 11:36:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+
+function calculateLength() {
+    var lengthInput = document.getElementById("lengthInput").value;
+    var fromUnit = document.getElementById("fromUnit").value;
+    var toUnit = document.getElementById("toUnit").value;
+    var result = 0;
+
+    // Perform the length conversion calculation
+    if (fromUnit === "inches") {
+      if (toUnit === "centimeters") {
+        result = lengthInput * 2.54;
+      } else if (toUnit === "feet") {
+        result = lengthInput / 12;
+      } else if (toUnit === "meters") {
+        result = lengthInput * 0.0254;
+      } else {
+        result = lengthInput;
+      }
+    } else if (fromUnit === "centimeters") {
+      if (toUnit === "inches") {
+        result = lengthInput * 0.393701;
+      } else if (toUnit === "feet") {
+        result = lengthInput * 0.0328084;
+      } else if (toUnit === "meters") {
+        result = lengthInput * 0.01;
+      } else {
+        result = lengthInput;
+      }
+    } else if (fromUnit === "feet") {
+      if (toUnit === "inches") {
+        result = lengthInput * 12;
+      } else if (toUnit === "centimeters") {
+        result = lengthInput * 30.48;
+      } else if (toUnit === "meters") {
+        result = lengthInput * 0.3048;
+      } else {
+        result = lengthInput;
+      }
+    } else if (fromUnit === "meters") {
+      if (toUnit === "inches") {
+        result = lengthInput * 39.3701;
+      } else if (toUnit === "centimeters") {
+        result = lengthInput * 100;
+      } else if (toUnit === "feet") {
+        result = lengthInput * 3.28084;
+      } else {
+        result = lengthInput;
+      }
+    }
+
+    document.getElementById("result").value = result;
+  }
